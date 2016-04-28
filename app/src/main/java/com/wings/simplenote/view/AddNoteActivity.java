54c64161055b405d5584ac0ba1eb79e3b75a826a
ExcaliburZ <419472353@qq.com>
2016-04-28 20:41:31 +0800

    Complete AddNoteActivity Layout.

--- app/src/main/java/com/wings/simplenote/view/AddNoteActivity.java
+++ app/src/main/java/com/wings/simplenote/view/AddNoteActivity.java
@@ -1,23 +1,82 @@
 package com.wings.simplenote.view;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.wings.simplenote.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class AddNoteActivity extends AppCompatActivity {
 
-    private static final String TAG = "AddNoteActivity";
     @Bind(R.id.toolbar)
     Toolbar mToolbar;
+    @Bind(R.id.ib_save)
+    ImageButton mIbSave;
+    @Bind(R.id.ib_trash)
+    ImageButton mIbTrash;
+    @Bind(R.id.et_title)
+    EditText mEtTitle;
+    @Bind(R.id.tv_date)
+    TextView mTvDate;
+    @Bind(R.id.tv_time)
+    TextView mTvTime;
+    @Bind(R.id.cb_alarm)
+    CheckBox mCbAlarm;
+    @Bind(R.id.et_content)
+    EditText mEtContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+        ButterKnife.bind(this);
+        initView();
+    }
+
+
+    private void initView() {
         setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toggleSoftKeyboard();
+    }
+
+    private void toggleSoftKeyboard() {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            public void run() {
+                InputMethodManager inputManager =
+                        (InputMethodManager) mEtTitle.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputManager.showSoftInput(mEtTitle, 0);
+            }
+        }, 998);
+    }
+
+    @OnClick({R.id.ib_save, R.id.ib_trash, R.id.tv_date, R.id.tv_time})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ib_save:
+                break;
+            case R.id.ib_trash:
+                break;
+            case R.id.tv_date:
+                break;
+            case R.id.tv_time:
+                break;
+        }
     }
 }
